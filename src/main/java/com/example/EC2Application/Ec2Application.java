2a96ad4aa2e1a2d45310ce6001f795f0b2c2b4dc
sibi-anand-vm <contacttosibi@gmail.com>
2026-09-16 22:48:52 +0530

    Added : Initial Working setup for Springboot EC2 Application

---
+++ src/main/java/com/example/EC2Application/Ec2Application.java
@@ -0,0 +1,13 @@
+package com.example.EC2Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EC2Application/Ec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EC2Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
